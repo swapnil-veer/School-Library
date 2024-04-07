1466d62f22238d7c66f811d243276250bca70bf7
SwapnilV <veerswapnil00@gmail.com>
2024-04-07 23:10:18 +0530

    add to borrow page added

--- book_app/views/librarian_views.py
+++ book_app/views/librarian_views.py
@@ -1,16 +1,17 @@
 from ..models import Borrow
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import user_passes_test
 from users.models import CustomUser
 from django.core.paginator import Paginator
 from django.views.generic.edit import CreateView
 from ..models import Book
 from ..forms import BookForm, BorrowForm
-from django.urls import reverse_lazy
+from django.urls import reverse_lazy, reverse
 from django.contrib.messages.views import SuccessMessageMixin
 from django.contrib.auth.mixins import UserPassesTestMixin
 from django.http import HttpResponseForbidden
-
+from django.contrib import messages
+from time import timezone
 
 def is_admin(user):
     return (user.is_authenticated and user.is_superuser)
@@ -50,18 +51,34 @@ def add_to_borrow(request):
     if request.method == "GET":
         pass
 
-class AddToBorrow(UserPassesTestMixin, SuccessMessageMixin, CreateView):
-    model = Borrow
-    form_class = BorrowForm
-    template_name = 'librarian/add_to_borrow.html/'
-    success_url = reverse_lazy('add_to_borrow')
-    success_message = "Student book added successfully."  # Success message to display
+# class AddToBorrow(UserPassesTestMixin, SuccessMessageMixin, CreateView):
+#     model = Borrow
+#     form_class = BorrowForm
+#     template_name = 'librarian/add_to_borrow.html/'
+#     success_url = reverse_lazy('add_to_borrow')
+#     success_message = "Student book added successfully."  # Success message to display
 
-    def get_success_url(self):
-        return reverse_lazy('add_to_borrow') 
+#     def get_success_url(self):
+#         return reverse_lazy('add_to_borrow') 
     
-    def test_func(self):
-        return is_admin(self.request.user)
+#     def test_func(self):
+#         return is_admin(self.request.user)
 
-    def handle_no_permission(self):
-        return HttpResponseForbidden("You do not have permission to access this page.")
+#     def handle_no_permission(self):
+#         return HttpResponseForbidden("You do not have permission to access this page.")
+
+@user_passes_test(is_admin)
+def add_to_borrow(request):
+    if request.method == 'POST':
+        form = BorrowForm(request.POST)
+        if form.is_valid():
+            borrow = form.save()
+            # borrow.due_date = timezone.now() + timezone.timedelta(days=30)
+            # borrow.save()
+            messages.success(request, "Student book added successfully.")
+            return redirect(reverse('add_to_borrow'))
+    else:
+        form = BorrowForm()
+    
+    context = {'form': form}
+    return render(request, 'librarian/add_to_borrow.html', context)
